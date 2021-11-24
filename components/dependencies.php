a01ededcae2f9e82e6ff3672a37f9f23afdda81a
Ricardo-we <totgamer75@gmail.com>
2021-11-24 11:22:20 -0600

    updated href in dependencies.php

--- components/dependencies.php
+++ components/dependencies.php
@@ -1,5 +1,5 @@
 <!-- BOOTSWATCH -->
-<link rel="stylesheet" href="/JEWELRY/static/dependencies/bootstrap.min.css">
+<link rel="stylesheet" href="../static/dependencies/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css"> -->
 
